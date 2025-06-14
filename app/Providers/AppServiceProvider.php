5fbba6b3a820a4aae8efdc78713c1bf9d6e191b3
rramadany <pv@subjective.work>
2025-06-14 12:29:57 +0300

    improvement: Use bootstrap pagination

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,5 +27,6 @@ class AppServiceProvider extends ServiceProvider
             $notifications = $user ? $user->unreadNotifications : collect();
             $view->with('unreadNotifications', $notifications);
         });
+        Paginator::useBootstrap();
     }
 }
